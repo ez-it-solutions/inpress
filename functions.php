edee85c43c27a0cda75230ae02a21a33d3b158d0
justliam <git@liam.lt>
2019-06-14 00:17:42 +0930

    Move sidebar to new file

--- functions.php
+++ functions.php
@@ -99,24 +99,6 @@ function inpress_content_width() {
 add_action( 'after_setup_theme', 'inpress_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function inpress_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'inpress' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'inpress' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'inpress_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function inpress_scripts() {
@@ -165,6 +147,10 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+
+// load sidebar
+require_once( 'library/sidebar.php' );
 
 // as per bones
 // LOAD BONES CORE (if you remove this, the theme will break)
