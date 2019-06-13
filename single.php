71a7f09c14d2dece6a457074445459b6f461bf1f
justliam <git@liam.lt>
2019-06-14 00:17:00 +0930

    Update single.php

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-<section>
+<section class="section">
 	<div class="container">
 
 		<?php
@@ -21,10 +21,7 @@ get_header();
 
 			the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			get_template_part( 'template-parts/comments', '' );
 
 		endwhile; // End of the loop.
 		?>
@@ -33,5 +30,4 @@ get_header();
 </section>
 
 <?php
-get_sidebar();
 get_footer();
