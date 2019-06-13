1a188868c957a7b21875188f08570e05b9a00115
justliam <git@liam.lt>
2019-06-14 00:17:02 +0930

    Update search.php

--- search.php
+++ search.php
@@ -1,55 +1,47 @@
-<?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package inpress
- */
-
+<?php /* search result template https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result */
 get_header();
 ?>
 
-<section>
+<section class="section">
 	<div class="container">
+		<div class="columns">
+			<!-- first column ***** -->
+			<div class="column is-two-thirds">
+				<?php 
+				if ( have_posts() ) : 
+				?>
 
-		<?php if ( have_posts() ) : ?>
+				<header class="page-header">
+					<h1 class="page-title">
+						<?php
+						/* translators: %s: search query. */
+						printf( esc_html__( 'Search Results for: %s', 'inpress' ), '<span>' . get_search_query() . '</span>' );
+						?>
+					</h1>
+				</header>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'inpress' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+				<?php
+				/* loop */
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content', 'search' );
+				endwhile;
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				the_posts_navigation();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				else :
+				get_template_part( 'template-parts/content', 'none' );
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+				endif;
+				?>
+			</div>
+			<!-- second column ***** -->
+			<div class="column is-one-third">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
 	</div>
 </section>
 
 <?php
-get_sidebar();
 get_footer();
