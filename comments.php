915d012a4b5ff7d6925adf044333770c5cd0733f
justliam <git@liam.lt>
2019-06-16 21:57:23 +0930

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,32 +1,19 @@
 <?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package inpress
- */
-
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
+/* comment template
+* If the current post is protected by a password andbthe visitor has not yet entered the password we willnreturn early without loading the comments.
+*/
 if ( post_password_required() ) {
 	return;
 }
 ?>
 
-<section class="section">
+<section class="section comments">
 	<div class="container">
 
 		<?php
-		// You can start editing here -- including this comment!
 		if ( have_comments() ) :
 			?>
+			<h1 class="title">Comments</h1>
 			<h2 class="comments-title">
 				<?php
 				$inpress_comment_count = get_comments_number();
@@ -34,46 +21,89 @@ if ( post_password_required() ) {
 					printf(
 						/* translators: 1: title. */
 						esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'inpress' ),
-						'<span>' . get_the_title() . '</span>'
+						'<span>' . esc_html( get_the_title() ) . '</span>'
 					);
 				} else {
 					printf( // WPCS: XSS OK.
 						/* translators: 1: comment count number, 2: title. */
 						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $inpress_comment_count, 'comments title', 'inpress' ) ),
-						number_format_i18n( $inpress_comment_count ),
-						'<span>' . get_the_title() . '</span>'
+						esc_html( number_format_i18n( $inpress_comment_count ) ),
+						'<span>' . esc_html( get_the_title() ) . '</span>'
 					);
 				}
 				?>
-			</h2><!-- .comments-title -->
+			</h2>
 
-			<?php the_comments_navigation(); ?>
+			<?php
+			/*
+			https://developer.wordpress.org/reference/functions/get_the_comments_navigation/
+			*/
+			the_comments_navigation(
+				array(
+					'prev_text'          => 'Older comments',
+					'next_text'          => 'Newer comments',
+					'screen_reader_text' => 'Comments navigation',
+				)
+			);
+			?>
 
 			<ol class="comment-list">
 				<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
+				// https://codex.wordpress.org/Function_Reference/wp_list_comments
+				wp_list_comments(
+					array(
+						'walker'            => null,
+						'max_depth'         => '',
+						'style'             => 'ol',
+						'callback'          => null,
+						'end-callback'      => null,
+						'type'              => 'all',
+						'reply_text'        => 'Reply',
+						'page'              => '',
+						'per_page'          => '',
+						'avatar_size'       => 0,
+						'reverse_top_level' => null,
+						'reverse_children'  => '',
+						'format'            => 'html5', // or 'xhtml' if no 'HTML5' theme support
+						'short_ping'        => true,   // @since 3.6
+						'echo'              => true,     // boolean, default is true
+					)
+				);
 				?>
-			</ol><!-- .comment-list -->
+			</ol>
 
 			<?php
+			// show navigation again
 			the_comments_navigation();
 
-			// If comments are closed and there are comments, let's leave a little note, shall we?
+			// if comments are closed
 			if ( ! comments_open() ) :
 				?>
 				<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'inpress' ); ?></p>
 				<?php
 			endif;
 
-		endif; // Check for have_comments().
-
-		comment_form();
+		endif;
+		/*
+		https://codex.wordpress.org/Function_Reference/comment_form
+		*/
+		comment_form(
+			array(
+				'id_form'           => 'commentform',
+				'class_form'        => 'comment-form',
+				'id_submit'         => 'submit',
+				'class_submit'      => 'submit',
+				'name_submit'       => 'submit',
+				'title_reply'       => __( 'Leave a Reply', 'inpress' ),
+				/* translators: comment */
+				'title_reply_to'    => __( 'Leave a Reply to %s', 'inpress' ),
+				'cancel_reply_link' => __( 'Cancel Reply', 'inpress' ),
+				'label_submit'      => __( 'Post Comment', 'inpress' ),
+				// more attributes for fields
+			)
+		);
 		?>
 
 	</div>
 </section>
 
-</div><!-- #comments -->
