154d91a1405d45ad7901b0b310f5b5e8c842e0e1
justliam <git@liam.lt>
2019-06-16 21:58:25 +0930

    Update search.php

--- search.php
+++ search.php
@@ -7,8 +7,8 @@ get_header();
 		<div class="columns">
 			<!-- first column ***** -->
 			<div class="column is-two-thirds">
-				<?php 
-				if ( have_posts() ) : 
+				<?php
+					if ( have_posts() ) :
 				?>
 
 				<header class="page-header">
@@ -21,18 +21,18 @@ get_header();
 				</header>
 
 				<?php
-				/* loop */
-				while ( have_posts() ) :
-					the_post();
-					get_template_part( 'template-parts/content', 'search' );
-				endwhile;
+						/* loop */
+						while ( have_posts() ) :
+							the_post();
+							get_template_part( 'template-parts/content', 'search' );
+						endwhile;
 
-				the_posts_navigation();
+						the_posts_navigation();
 
-				else :
-				get_template_part( 'template-parts/content', 'none' );
+						else :
+						get_template_part( 'template-parts/content', 'none' );
 
-				endif;
+					endif;
 				?>
 			</div>
 			<!-- second column ***** -->
