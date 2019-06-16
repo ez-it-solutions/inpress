e03502465ffe7346ac1f750bc4c2cef1156c1c0f
justliam <git@liam.lt>
2019-06-16 21:56:42 +0930

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,16 +1,24 @@
-<?php /* 404 template https://codex.wordpress.org/Creating_an_Error_404_Page */
+<?php
+/*
+404 template
+todo:
+- list similar pages/sitemap/search results?
+https://codex.wordpress.org/Creating_an_Error_404_Page
+https://codex.wordpress.org/Function_Reference/is_404
+*/
 get_header();
 ?>
 
-<section class="section">
+<section class="section 404">
 	<div class="container">
-		<section class="error-404 not-found">
-			<h1 class="title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'inpress' ); ?></h1>
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'inpress' ); ?></p>
-				<?php get_search_form(); ?>
-			</div>
-		</section>
+		<h1 class="title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'inpress' ); ?></h1>
+		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'inpress' ); ?></p>
+		<?php
+		/*
+			https://developer.wordpress.org/reference/functions/get_search_form/
+		*/
+		get_search_form();
+		?>
 	</div>
 </section>
 
