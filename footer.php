d8f766cbdf1976de829220a749533b5011f341ca
justliam <git@liam.lt>
2019-06-16 21:57:25 +0930

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,20 @@
-<?php /* footer template partial */ ?>
+<?php /* footer template partial, included with get_footer();
+https://developer.wordpress.org/reference/functions/get_footer/ */ ?>
 
+<?php /* bulma https://bulma.io/documentation/layout/footer/ */ ?>
 <footer class="footer">
-	<section>
+	<section class="section">
 		<div class="container">
-			<?php /* footer */ ?>
+			<?php /* the footer */ ?>
 		</div>
 	</section>
 </footer>
 
-<?php wp_footer(); ?>
+<?php
+wp_footer();
+// https://developer.wordpress.org/reference/functions/wp_footer/
+// https://codex.wordpress.org/Plugin_API/Action_Reference/wp_footer
+?>
 
 </body>
 </html>
