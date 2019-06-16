56714f3c1dff948e3d78d486697fda242ec28337
justliam <git@liam.lt>
2019-06-16 21:57:30 +0930

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php /* header template partial */ ?>
-
 <!doctype html>
 <html <?php language_attributes(); ?>>
 	<head>
@@ -8,19 +7,17 @@
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<?php /* https://bulma.io */ ?>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
-		<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>	
+		<?php /* <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script> */ ?>
 		<?php wp_head(); ?>
 	</head>
 <body <?php body_class(); ?>>
-
 <?php /* https://bulma.io/documentation/layout/hero */ ?>
-
-<section class="hero is-primary">
-  <div class="hero-body">
-    <div class="container">
-    	<h1 class="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-	 	<h2 class="subtitle">Hero subtitle</h2>
-		<?php get_template_part( 'template-parts/navigation', 'none' ); ?>
-    </div>
-  </div>
+<section class="hero is-info is-bold">
+	<div class="hero-body">
+		<div class="container">
+			<h1 class="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="subtitle">Hero subtitle</h2>
+			<?php get_template_part( 'template-parts/navigation', 'none' ); ?>
+		</div>
+	</div>
 </section>
