0b2d51aae0260ad2259fcc894eafa2d9262dfe53
justliam <git@liam.lt>
2019-06-16 21:57:54 +0930

    Update inpress.php

--- library/inpress.php
+++ library/inpress.php
@@ -8,20 +8,20 @@ in the functions.php file.
 Developed by: Eddie Machado
 URL: http://themble.com/bones/
 
-  - head cleanup (remove rsd, uri links, junk css, ect)
-  - enqueueing scripts & styles
-  - theme support functions
-  - custom menu output & fallbacks
-  - related post function
-  - page-navi function
-  - removing <p> from around images
-  - customizing the post excerpt
+- head cleanup (remove rsd, uri links, junk css, ect)
+- enqueueing scripts & styles
+- theme support functions
+- custom menu output & fallbacks
+- related post function
+- page-navi function
+- removing <p> from around images
+- customizing the post excerpt
 
 */
 
 /*********************
 WP_HEAD GOODNESS
-The default wordpress head is
+The default wp head is
 a mess. Let's clean it up by
 removing all the junk we don't
 need.
@@ -31,31 +31,32 @@ need.
 // A better title
 // http://www.deluxeblogtips.com/2012/03/better-title-meta-tag.html
 function rw_title( $title, $sep, $seplocation ) {
-  global $page, $paged;
+	global $page, $paged;
 
-  // Don't affect in feeds.
-  if ( is_feed() ) return $title;
+	// Don't affect in feeds.
+	if ( is_feed() ) return $title;
 
-  // Add the blog's name
-  if ( 'right' == $seplocation ) {
-    $title .= get_bloginfo( 'name' );
-  } else {
-    $title = get_bloginfo( 'name' ) . $title;
-  }
+	// Add the blog's name
+	if ( 'right' == $seplocation ) {
+		$title .= get_bloginfo( 'name' );
+	} else {
+		$title = get_bloginfo( 'name' ) . $title;
+	}
 
-  // Add the blog description for the home/front page.
-  $site_description = get_bloginfo( 'description', 'display' );
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
 
-  if ( $site_description && ( is_home() || is_front_page() ) ) {
-    $title .= " {$sep} {$site_description}";
-  }
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title .= " {$sep} {$site_description}";
+	}
 
-  // Add a page number if necessary:
-  if ( $paged >= 2 || $page >= 2 ) {
-    $title .= " {$sep} " . sprintf( __( 'Page %s', 'dbt' ), max( $paged, $page ) );
-  }
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 ) {
+		/* translators: to do */
+		$title .= " {$sep} " . sprintf( __( 'Page %s', 'inpress' ), max( $paged, $page ) );
+	}
 
-  return $title;
+	return $title;
 
 } // end better title
 
@@ -91,117 +92,6 @@ function bones_gallery_style($css) {
 
 
 /*********************
-SCRIPTS & ENQUEUEING
-*********************/
-
-// loading modernizr and jquery, and reply script
-function bones_scripts_and_styles() {
-
-  global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
-
-  if (!is_admin()) {
-
-		// modernizr (without media query polyfill)
-		wp_register_script( 'bones-modernizr', get_stylesheet_directory_uri() . '/library/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
-
-		// register main stylesheet
-		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
-
-		// ie-only style sheet
-		wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
-
-    // comment reply script for threaded comments
-    if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
-		  wp_enqueue_script( 'comment-reply' );
-    }
-
-		//adding scripts file in the footer
-		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
-
-		// enqueue styles and scripts
-		wp_enqueue_script( 'bones-modernizr' );
-		wp_enqueue_style( 'bones-stylesheet' );
-		wp_enqueue_style( 'bones-ie-only' );
-
-		$wp_styles->add_data( 'bones-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
-
-		/*
-		I recommend using a plugin to call jQuery
-		using the google cdn. That way it stays cached
-		and your site will load faster.
-		*/
-		wp_enqueue_script( 'jquery' );
-		wp_enqueue_script( 'bones-js' );
-
-	}
-}
-
-/*********************
-THEME SUPPORT
-*********************/
-
-// Adding WP 3+ Functions & Theme Support
-function bones_theme_support() {
-
-	// wp thumbnails (sizes handled in functions.php)
-	add_theme_support( 'post-thumbnails' );
-
-	// default thumb size
-	set_post_thumbnail_size(125, 125, true);
-
-	// wp custom background (thx to @bransonwerner for update)
-	add_theme_support( 'custom-background',
-	    array(
-	    'default-image' => '',    // background image default
-	    'default-color' => '',    // background color default (dont add the #)
-	    'wp-head-callback' => '_custom_background_cb',
-	    'admin-head-callback' => '',
-	    'admin-preview-callback' => ''
-	    )
-	);
-
-	// rss thingy
-	add_theme_support('automatic-feed-links');
-
-	// to add header image support go here: http://themble.com/support/adding-header-background-image-support/
-
-	// adding post format support
-	add_theme_support( 'post-formats',
-		array(
-			'aside',             // title less blurb
-			'gallery',           // gallery of images
-			'link',              // quick link to other site
-			'image',             // an image
-			'quote',             // a quick quote
-			'status',            // a Facebook like status update
-			'video',             // video
-			'audio',             // audio
-			'chat'               // chat transcript
-		)
-	);
-
-	// wp menus
-	add_theme_support( 'menus' );
-
-	// registering wp3+ menus
-	register_nav_menus(
-		array(
-			'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
-			'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
-		)
-	);
-
-	// Enable support for HTML5 markup.
-	add_theme_support( 'html5', array(
-		'comment-list',
-		'search-form',
-		'comment-form'
-	) );
-
-} /* end bones theme support */
-
-
-/*********************
 RELATED POSTS FUNCTION
 *********************/
 
@@ -223,9 +113,11 @@ function bones_related_posts() {
 		if($related_posts) {
 			foreach ( $related_posts as $post ) : setup_postdata( $post ); ?>
 				<li class="related_post"><a class="entry-unrelated" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-			<?php endforeach; }
-		else { ?>
-			<?php echo '<li class="no_related_post">' . __( 'No Related Posts Yet!', 'bonestheme' ) . '</li>'; ?>
+			<?php
+			endforeach; }
+		else { 
+		?>
+			<?php echo '<li class="no_related_post">' . __( 'No Related Posts Yet!', 'inpress' ) . '</li>'; ?>
 		<?php }
 	}
 	wp_reset_postdata();
@@ -238,23 +130,25 @@ PAGE NAVI
 
 // Numeric Page Navi (built into the theme by default)
 function bones_page_navi() {
-  global $wp_query;
-  $bignum = 999999999;
-  if ( $wp_query->max_num_pages <= 1 )
-    return;
-  echo '<nav class="pagination">';
-  echo paginate_links( array(
-    'base'         => str_replace( $bignum, '%#%', esc_url( get_pagenum_link($bignum) ) ),
-    'format'       => '',
-    'current'      => max( 1, get_query_var('paged') ),
-    'total'        => $wp_query->max_num_pages,
-    'prev_text'    => '&larr;',
-    'next_text'    => '&rarr;',
-    'type'         => 'list',
-    'end_size'     => 3,
-    'mid_size'     => 3
-  ) );
-  echo '</nav>';
+	global $wp_query;
+	$bignum = 999999999;
+	if ( $wp_query->max_num_pages <= 1 )
+	return;
+	echo '<nav class="pagination">';
+	echo paginate_links(
+		array(
+			'base'      => str_replace( $bignum, '%#%', esc_url( get_pagenum_link( $bignum ) ) ),
+			'format'    => '',
+			'current'   => max( 1, get_query_var( 'paged' ) ),
+			'total'     => $wp_query->max_num_pages,
+			'prev_text' => '&larr;',
+			'next_text' => '&rarr;',
+			'type'      => 'list',
+			'end_size'  => 3,
+			'mid_size'  => 3,
+		)
+	);
+	echo '</nav>';
 } /* end page navi */
 
 ?>
