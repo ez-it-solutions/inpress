89b48a60d4c62127a7c98ec52c3127345fe2955e
justliam <git@liam.lt>
2019-06-17 17:28:35 +0930

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * inpress functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package inpress
- */
-
-
+/* https://developer.wordpress.org/themes/basics/theme-functions/ */
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -24,18 +16,14 @@ function inpress_content_width() {
 }
 add_action( 'after_setup_theme', 'inpress_content_width', 0 );
 
-
 /** Implement the Custom Header feature. */
-require get_template_directory() . '/inc/custom-header.php';
-
+//require get_template_directory() . '/inc/custom-header.php';
 /** Custom template tags for this theme. */
 require get_template_directory() . '/inc/template-tags.php';
-
 /* Functions which enhance the theme by hooking into WordPress. */
-require get_template_directory() . '/inc/template-functions.php';
-
+//require get_template_directory() . '/inc/template-functions.php';
 /* Customizer additions. */
-require get_template_directory() . '/inc/customizer.php';
+//require get_template_directory() . '/inc/customizer.php';
 
 /* Load Jetpack compatibility file. */
 if ( defined( 'JETPACK__VERSION' ) ) {
@@ -49,19 +37,16 @@ if ( class_exists( 'WooCommerce' ) ) {
 
 // load theme support
 require_once( 'library/support.php' );
-
 // load scripts
 require_once( 'library/scripts.php' );
-
 // load sidebar
 require_once( 'library/sidebar.php' );
-
-// as per bones
-// LOAD BONES CORE (if you remove this, the theme will break)
+// as per bones LOAD BONES CORE 
 require_once( 'library/inpress.php' );
-
+require_once( 'library/emoji.php' );
 // move cleanup to own file
 require_once( 'library/cleanup.php' );
-
 // CUSTOMIZE THE WordPress ADMIN (off by default)
 require_once( 'library/admin.php' );
+
+remove_action('wp_head', 'wp_generator');
