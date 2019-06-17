8acb016d03a70e014ba2bc7a68ae28df64d4694e
justliam <git@liam.lt>
2019-06-17 17:28:00 +0930

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,11 +1,14 @@
 <?php
-/*
-404 template
+/** 404 template
 todo:
 - list similar pages/sitemap/search results?
 https://codex.wordpress.org/Creating_an_Error_404_Page
 https://codex.wordpress.org/Function_Reference/is_404
-*/
+
+@package inpress */
+
+?>
+
 get_header();
 ?>
 
@@ -14,9 +17,7 @@ get_header();
 		<h1 class="title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'inpress' ); ?></h1>
 		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'inpress' ); ?></p>
 		<?php
-		/*
-			https://developer.wordpress.org/reference/functions/get_search_form/
-		*/
+		/* https://developer.wordpress.org/reference/functions/get_search_form/ */
 		get_search_form();
 		?>
 	</div>
