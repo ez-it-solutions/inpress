e59608d32d02893c66fbb9783b57e3d466959998
justliam <git@liam.lt>
2019-06-17 17:28:18 +0930

    Update single.php

--- single.php
+++ single.php
@@ -1,5 +1,9 @@
 <?php
-/* Single post template https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post */
+/** Single post template https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+
+@package inpress
+ */
+
 get_header();
 ?>
 
@@ -12,6 +16,7 @@ get_header();
 			get_template_part( 'template-parts/content', get_post_type() );
 
 			/*
+			Notes
 			https://developer.wordpress.org/reference/functions/the_post_navigation/
 			https://developer.wordpress.org/reference/functions/get_the_post_navigation/
 			Displays the navigation to next/previous post, when applicable.
@@ -21,14 +26,16 @@ get_header();
 			.nav-previous a
 			.nav-next a
 			*/
-			the_post_navigation( array(
-				'prev_text'          => '%title',
-				'next_text'          => '%title',
-				'in_same_term'       => false,
-				'excluded_terms'     => '',
-				'taxonomy'           => 'category',
-				'screen_reader_text' => __( 'Post navigation' ),
-			) );
+			the_post_navigation(
+				array(
+					'prev_text'          => '%title',
+					'next_text'          => '%title',
+					'in_same_term'       => false,
+					'excluded_terms'     => '',
+					'taxonomy'           => 'category',
+					'screen_reader_text' => __( 'Post navigation' ),
+				)
+			);
 
 			get_template_part( 'template-parts/comments', '' );
 		endwhile;
