d4293e46bbcb6846a47db90302775c40055e5382
justliam <git@liam.lt>
2019-06-17 17:28:13 +0930

    Update page.php

--- page.php
+++ page.php
@@ -1,11 +1,13 @@
 <?php
-/* template for pages */
+/** Template for pages
+
+@package inpress
+ */
+
 get_header();
 ?>
-
 <section class="section">
 	<div class="container">
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -13,10 +15,8 @@ get_header();
 			get_template_part( 'template-parts/comments', '' );
 		endwhile;
 		?>
-
 	</div>
 </section>
-
 <?php
-//get_sidebar();
+/* get_sidebar(); */
 get_footer();
